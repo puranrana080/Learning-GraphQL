41cc6dd6388f9f484daa1f07685937280aec9a48
Puran Singh Rana <puransinghrana080@gmail.com>
2025-04-27 00:26:02 +0530

    define resolvers

--- index.js
+++ index.js
@@ -1,17 +1,33 @@
 import { ApolloServer } from "@apollo/server";
 import { startStandaloneServer } from "@apollo/server/standalone";
+import db from "./_db.js";
 //typeDefs
-import { typeDefs } from "./schema";
+import { typeDefs } from "./schema.js";
+//resolvers
+const resolvers = {
+  Query: {
+    reviews() {
+      return db.reviews;
+    },
+    games() {
+      return db.games;
+    },
+    authors() {
+      return db.authors;
+    },
+  },
+};
 
 //server setup
-const server=new ApolloServer({typeDefs
-    //typeDefs
-    //resolvers
-})
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  //typeDefs
+  //resolvers
+});
 
+const { url } = await startStandaloneServer(server, {
+  listen: { port: 4000 },
+});
 
-const {url}=await startStandaloneServer(server,{
-    listen:{port:4000}
-})
-
-console.log("Server ready at port ",4000)
+console.log("Server ready at port ", 4000);
