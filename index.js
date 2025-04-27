f45478b0535b077f09b142a24d8ab0429efeb34f
Puran Singh Rana <puransinghrana080@gmail.com>
2025-04-27 17:49:42 +0530

    mutation, update

--- index.js
+++ index.js
@@ -56,6 +56,15 @@ Mutation:{
         let game={...args.game,id:Math.floor(Math.random()*10000).toString()}
         db.games.push(game)
         return game
+    },
+    updateGame(_,args){
+        db.games=db.games.map((g)=>{
+            if(g.id===args.id){
+                return {...g,...args.edits}
+            }
+            return g
+        })
+        return db.games.find((g)=>g.id===args.id)
     }
 }
 
